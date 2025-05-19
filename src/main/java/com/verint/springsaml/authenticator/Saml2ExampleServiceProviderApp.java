6d5e688de8a73f73daa1c10d77bc8bf26559e485
Vi Tran <vianh.tran@verint.com>
2025-05-18 23:40:47 -0400

    Revert "build war for weblogic"
    looks like it can not auto wire the RelyingPartyRegistrationRepository

--- src/main/java/com/verint/springsaml/authenticator/Saml2ExampleServiceProviderApp.java
+++ src/main/java/com/verint/springsaml/authenticator/Saml2ExampleServiceProviderApp.java
@@ -2,16 +2,9 @@ package com.verint.springsaml.authenticator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Saml2ExampleServiceProviderApp extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
-        return application.sources(Saml2ExampleServiceProviderApp.class);
-    }
+public class Saml2ExampleServiceProviderApp {
 
     public static void main(String[] args) {
         SpringApplication.run(Saml2ExampleServiceProviderApp.class, args);
